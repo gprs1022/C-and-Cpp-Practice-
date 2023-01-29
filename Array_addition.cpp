4e219b6166b5854b7a5c8d68873859a85270da9f
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-01-29 08:08:54 +0530

    Array Addition

---
+++ Array_addition.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int arr[100], size, i, sum = 0;
+    
+    
+    cout<<"Enter array size\n";
+    cin>>size;
+    
+   
+    cout<<"Enter array elements"<<endl;
+    for(i = 0; i < size; i++)
+          cin>>arr[i];
+          
+    
+    for(i = 0; i < size; i++)
+          sum = sum + arr[i]; 
+    
+  
+    cout<<"Sum of the array"<<sum;
+    
+    return 0;
+}
