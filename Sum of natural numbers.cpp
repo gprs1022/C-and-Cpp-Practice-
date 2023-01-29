e1397380865373dfe946373e7a12e619e4caf687
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-01-29 08:09:29 +0530

    Sum of natural numbers

---
+++ Sum of natural numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,sum=0;
+	cout<<"Naturals numbers"<<endl;
+	for(i=0;i<=10;i++)
+	{
+		cout<<i;
+		sum=sum+i;
+	}
+	cout<<"the sum of natural number"<<sum;
+	return 0;
+}
+
+
+	
